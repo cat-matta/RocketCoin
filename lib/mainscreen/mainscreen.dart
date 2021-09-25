17a0edc15e09044bde029a91527ce5bae08ef3b1
Catherine <catherine.matta22399@gmail.com>
2021-09-25 11:18:42 -0400

    Google Fonts added

--- lib/mainscreen/mainscreen.dart
+++ lib/mainscreen/mainscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -7,6 +8,21 @@ class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
 }
+
+final List<List<String>> items = [
+  ["item1", "23"],
+  ["item3", "21"],
+  ["item2", "20"],
+  ["item4", "22"],
+  ["item5", "200"],
+  ["item6", "211"],
+  ["item7", "2211"],
+  ["item8", "2212"],
+  ["item5", "200"],
+  ["item6", "211"],
+  ["item7", "2211"],
+  ["item8", "2212"]
+];
 
 class _MainScreenState extends State<MainScreen> {
   @override
@@ -22,6 +38,24 @@ class _MainScreenState extends State<MainScreen> {
                 image: AssetImage('assets/images/rocketcoin.png'),
                 height: 200,
               ),
+              SizedBox(
+                height: 60,
+                width: 150,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(15),
+                      backgroundColor: Color.fromRGBO(40, 173, 241, 1)),
+                  onPressed: () {},
+                  child: Text(
+                    "Add Transaction",
+                    style: GoogleFonts.spaceGrotesk(
+                        textStyle:
+                            TextStyle(color: Color.fromRGBO(18, 30, 98, 1))),
+                  ),
+                ),
+              ),
+              Spacer(),
+              TransactionsList(),
               Spacer(),
               SleekCircularSlider(
                   min: 0,
@@ -46,6 +80,57 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class TransactionsList extends StatefulWidget {
+  const TransactionsList({Key? key}) : super(key: key);
+
+  @override
+  _TransactionsListState createState() => _TransactionsListState();
+}
+
+class _TransactionsListState extends State<TransactionsList> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      decoration: BoxDecoration(
+          // color: Colors.white.withOpacity(1),
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(18, 30, 98, 1),
+            Color.fromRGBO(18, 30, 98, 1)
+          ]),
+          borderRadius: BorderRadius.circular(16)),
+      padding: EdgeInsets.all(8.0),
+      height: size.height / 4,
+      width: size.width - 40,
+      child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Row(
+                children: [
+                  Text(items[index][0],
+                      style: GoogleFonts.spaceGrotesk(
+                        textStyle: TextStyle(
+                            color: Color.fromRGBO(255, 196, 255, 1),
+                            fontSize: 20,
+                            fontWeight: FontWeight.normal),
+                      )),
+                  Spacer(),
+                  Text("\$" + items[index][1],
+                      style: GoogleFonts.spaceGrotesk(
+                          textStyle: TextStyle(
+                              color: Color.fromRGBO(255, 196, 255, 1),
+                              fontSize: 20,
+                              fontWeight: FontWeight.normal))),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
