89fcc127d073e96321e4ed14f065b7bf2504fad3
Catherine <catherine.matta22399@gmail.com>
2021-09-25 10:53:22 -0400

    Slider improvements

--- lib/mainscreen/mainscreen.dart
+++ lib/mainscreen/mainscreen.dart
@@ -18,16 +18,30 @@ class _MainScreenState extends State<MainScreen> {
           padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
-              SleekCircularSlider(
-                  appearance: CircularSliderAppearance(),
-                  onChange: (double value) {
-                    print(value);
-                  }),
-              Spacer(),
               Image(
                 image: AssetImage('assets/images/rocketcoin.png'),
                 height: 200,
               ),
+              Spacer(),
+              SleekCircularSlider(
+                  min: 0,
+                  max: 1000,
+                  initialValue: 500,
+                  appearance: CircularSliderAppearance(
+                      customColors: CustomSliderColors(
+                          progressBarColor: Color.fromRGBO(255, 196, 255, 1)),
+                      infoProperties: InfoProperties(
+                          modifier: (double value) {
+                            final roundedvalue =
+                                value.ceil().toInt().toString();
+                            return "$roundedvalue";
+                          },
+                          mainLabelStyle: TextStyle(
+                              color: Color.fromRGBO(255, 196, 255, 1),
+                              fontSize: 35))),
+                  onChange: (double value) {
+                    print(value);
+                  }),
             ],
           ),
         ),
